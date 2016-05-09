28a35d517fce9ab16cbe5eaeda05ca3a705f7f63
Ryan Burbank <rburbank@streamsend.com>
2016-05-08 21:19:27 -0700

    Implement FixedArray

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,20 @@
-class FixedArray
+class OutOfBoundsException < StandardError; end
 
+class FixedArray
+  attr_reader :array
+
+  def initialize(size)
+    @array = {}
+    size.times {|i| @array[i] = nil }
+  end
+
+  def get(index)
+    raise OutOfBoundsException if array.length < index
+    @array[index]
+  end
+
+  def set(index, value)
+    raise OutOfBoundsException if array.length < index
+    @array[index] = value
+  end
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,45 @@
-require_relative 'fixed_array'
+require_relative "fixed_array"
+require "pry"
 
 describe FixedArray do
-  
+  let(:size) { 5 }
+  let(:fixed_array) { FixedArray.new(size) }
+
+  describe "#new" do
+    it "is instatiated with space for 'size' elements" do
+      expect(fixed_array.array.length).to be size
+    end
+  end
+
+  describe "#get" do
+    let(:test_value) { "A test value" }
+
+    context "there is no element at the specified index" do
+      let(:out_of_bounds) { size + 1 }
+
+      it "throws an OutOfBoundsException" do
+        expect { fixed_array.get(out_of_bounds) }.to raise_error(OutOfBoundsException)
+      end
+    end
+  end
+
+  describe "#set" do
+    let(:test_value) { "A test value" }
+
+    context "there is an element at the specified index" do
+      it "sets a value in array at the specified index with the specified value" do
+        expect(fixed_array.get(0)).to be_nil
+        fixed_array.set(0, test_value)
+        expect(fixed_array.get(0)).to eq test_value
+      end
+    end
+
+    context "there is no element at the specified index" do
+      let(:out_of_bounds) { size + 1 }
+
+      it "throws an OutOfBoundsException" do
+        expect { fixed_array.set(out_of_bounds, test_value) }.to raise_error(OutOfBoundsException)
+      end
+    end
+  end
 end
